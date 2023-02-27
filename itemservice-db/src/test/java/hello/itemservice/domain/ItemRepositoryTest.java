01f909adcb62beb9265662e30106e44002cb4202
isyoo1213 <moethns@gmail.com>
2023-02-27 15:01:18 +0900

    * feat: Add Settings for Test using Transaction

--- itemservice-db/src/test/java/hello/itemservice/domain/ItemRepositoryTest.java
+++ itemservice-db/src/test/java/hello/itemservice/domain/ItemRepositoryTest.java
@@ -5,13 +5,23 @@ import hello.itemservice.repository.ItemSearchCond;
 import hello.itemservice.repository.ItemUpdateDto;
 import hello.itemservice.repository.memory.MemoryItemRepository;
 import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.transaction.TransactionStatus;
+import org.springframework.transaction.support.DefaultTransactionDefinition;
 
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
+
+// * @SpringBootTest
+// - 해당 어노테이션은 @SpringBootApplication 어노테이션이 붙은 클래스를 탐색
+// - 해당 클래스의 Config를 설정으로 사용
+// - 이전까진 @SpringBootApplication가 붙은 어플리케이션 클래스에서 MemoryRepository를 @Import()하고 있었지만
+//   현재는 JdbcTemplateRepositoryV3를 @Import()하고 있으므로 V3 레포지토리를 주입받아 사용함
 
 @SpringBootTest
 class ItemRepositoryTest {
@@ -20,6 +30,22 @@ class ItemRepositoryTest {
 
     @Autowired
     ItemRepository itemRepository;
+
+    // * Transaction 사용을 위한 의존성
+    // + * Datasource와 transactionManager는 Spring이 자동으로 Bean 등록해줌
+    @Autowired
+    PlatformTransactionManager transactionManager;
+
+    TransactionStatus status; // Rollback 설정에 필요하므로 필드로 선언만 하고 이후에 생성해주는 방식
+
+    // * 모든 Test 실행 전에 status를 생성하기 -> Transaction 시작해버리기
+    @BeforeEach
+    void beforeEach() {
+        // transactionManager에서 status 가져와버리기
+        // *** transaction을 시작하면 얻는 이점
+        // 굳이 commit하지 않아도 나의 session에서는 수행된 DB데이터들을 조회하는 것은 가능
+        status = transactionManager.getTransaction(new DefaultTransactionDefinition());
+    }
 
     @AfterEach
     void afterEach() {
@@ -30,6 +56,10 @@ class ItemRepositoryTest {
         if (itemRepository instanceof MemoryItemRepository) {
             ((MemoryItemRepository) itemRepository).clearStore();
         }
+
+        //Transaction 롤백
+        // * 현재 롤백 로그에는 JdbcTranscationManager를 설정했지만 실질적으로는 DatasourceTransactionManager를 상속
+        transactionManager.rollback(status);
     }
 
     @Test
@@ -39,6 +69,8 @@ class ItemRepositoryTest {
 
         //when
         Item savedItem = itemRepository.save(item);
+        // *** 실제 주입된 repository의 jdbcInsert는 트랜잭션 동기화 매니저 내에 저장된 con을 사용함
+        // -> 동일한 트랜잭션 내에서는 동일한 con으로 로직 수행
 
         //then
         Item findItem = itemRepository.findById(item.getId()).get();
