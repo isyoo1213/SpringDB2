0338cd73eeac426f1c7061fe1a4d020ffb99c03c
isyoo1213 <moethns@gmail.com>
2023-02-28 13:39:41 +0900

    feat: Add Bean for Embedded DB Test

--- itemservice-db/src/main/java/hello/itemservice/ItemServiceApplication.java
+++ itemservice-db/src/main/java/hello/itemservice/ItemServiceApplication.java
@@ -2,11 +2,15 @@ package hello.itemservice;
 
 import hello.itemservice.config.*;
 import hello.itemservice.repository.ItemRepository;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Import;
 import org.springframework.context.annotation.Profile;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+
+import javax.sql.DataSource;
 
 @Import(JdbcTemplateV3Config.class)
 //@Import(JdbcTemplateV2Config.class)
@@ -23,6 +27,7 @@ import org.springframework.context.annotation.Profile;
 //    -> *** @Import 어노테이션을 통해 MemoryConfig.class 또한 Bean등록이 가능한 Configuration으로 작동하도록 설정
 // *** 즉, ComponentScan이 적용되는 위치 외에서의 Configuration의 Bean등록을 수동으로 지정해주는 것
 @SpringBootApplication(scanBasePackages = "hello.itemservice.web")
+@Slf4j
 public class ItemServiceApplication {
 
 	public static void main(String[] args) {
@@ -51,6 +56,23 @@ public class ItemServiceApplication {
 	//    + ApplicationReadyEvent.class의 이벤트를 읽지 못하므로 초기 데이터가 없는 것을 확인 가능
 	public TestDataInit testDataInit(ItemRepository itemRepository) {
 		return new TestDataInit(itemRepository);
+	}
+
+	// *** Embedded 모드 DB를 위한 DataSource Bean을 수동 등록
+	// - JVM 내에 h2 DB를 만들고 사용
+	// - Test 수행 시, DataSource에 자동으로 주입됨
+	@Bean
+	@Profile("test") //프로필이 Test일 경우에만 해당 Bean을 등록
+	public DataSource dataSource() {
+		log.info("메모리 데이터베이스 초기화");
+		DriverManagerDataSource dataSource = new DriverManagerDataSource();
+		dataSource.setDriverClassName("org.h2.Driver");
+		dataSource.setUrl("jdbc:h2:mem:db;DB_CLOSE_DELAY=-1");
+		//jdbc:h2:mem:db - memory 모드
+		//DB_CLOSE_DELAY=-1 - con이 끊어질 경우 자동으로 메모리DB를 종료시키는 것을 막기 위한 설정
+		dataSource.setUsername("sa");
+		dataSource.setPassword("");
+		return dataSource;
 	}
 
 }
