ad89a0c182eef3bcb6fa172c3ffac1746d5584f6
isyoo1213 <moethns@gmail.com>
2023-03-04 23:52:10 +0900

    feat: Add  Repo/Config for basic SpringDataJpa

---
+++ itemservice-db/src/main/java/hello/itemservice/repository/jpa/JpaItemRepositoryV2.java
@@ -0,0 +1,86 @@
+package hello.itemservice.repository.jpa;
+
+import hello.itemservice.domain.Item;
+import hello.itemservice.repository.ItemRepository;
+import hello.itemservice.repository.ItemSearchCond;
+import hello.itemservice.repository.ItemUpdateDto;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.util.StringUtils;
+
+import javax.persistence.EntityManager;
+import javax.persistence.TypedQuery;
+import java.util.List;
+import java.util.Optional;
+
+/**
+ * * SpringDataJpa를 활용한 Repository는 ItemRepository 인터페이스를 구현하고 있지 않음
+ *    - Service 계층에서 Repository를 주입할 경우, ItemRepository가 아닌 SPJ Repository를 추가적으로 의존성 주입해주어야함
+ *    -> Service 계층이 새로운 의존관계를 형성하며 메서드의 내부로직을 SPJ에 맞게 전부 새로 짜주어야함
+ * * 해결방법
+ *    - Service 계층에서 곧장 SPJ 인터페이스를 의존받지 않고, 기존 ItemRepository를 SPJ로 구현하는 Repository를 추가
+ *    -> Service 계층 입장에서는 기존대로 ItemRepository를 주입받으므로, SPJ를 위한 추가적인 의존관계 형성을 피할 수 있음
+ * * 실제로는 해당 Repository의 Proxy가 실제 구현체가 되어 Bean등록되고, Runtime 객체 의존관계를 형성
+ *    - Runtime 객체 의존관계
+ *    -> itemService (인터페이스) -> jpaItemRepositoryV2 (구현체) -> (의존)<<Proxy>>springDataJpaItemRepository (Proxy객체)
+ * * 즉, JpaItemRepository가 어댑터와 같은 역할을 함으로써 Service계층의 코드를 건드리지 않아도 됨
+ */
+
+@Slf4j
+@Repository
+@Transactional
+@RequiredArgsConstructor
+public class JpaItemRepositoryV2 implements ItemRepository {
+
+    private final SpringDataJpaItemRepository repository; // @RequiredArgsConstructor로 바로 주입되도록 만듦
+
+
+    @Override
+    public Item save(Item item) {
+        return repository.save(item);
+        // *** 기본적인 CRUD 메서드는 JPA가 아닌 DATA의 CrudRepository 인터페이스에 규격
+        // *** JpaRepository 인터페이스는 상속관계를 올라가다보면 이 DATA의 CrudRepository를 상속하고 있음
+        // -> 실질적인 구현체인 '클래스'는 SimpleJpaRepository
+        // -> 결국 EntityManager인 em.persist() 메서드가 호출됨
+    }
+
+    @Override
+    public void update(Long itemId, ItemUpdateDto updateParam) {
+        Item findItem = repository.findById(itemId).orElseThrow();
+        //findById()의 반환형이 Optional이므로 orElseThrow()처리
+
+        //JPA에서의 update()는 메서드를 호출하는 것이 아닌 값만 세팅해주면 된다 - Commit 실행 시점에 DB에 반영
+        findItem.setItemName(updateParam.getItemName());
+        findItem.setPrice(updateParam.getPrice());
+        findItem.setQuantity(updateParam.getQuantity());
+    }
+
+    @Override
+    public Optional<Item> findById(Long id) {
+        return repository.findById(id);
+        // CrudRepository의 구현체인 SimpleJpaRepository의 findById()메서드를 사용
+        // + 반환타입 또한 Optional<T> -> 바로 리턴가능
+    }
+
+    @Override
+    public List<Item> findAll(ItemSearchCond cond) {
+        // 지금은 그냥 SDJ가 아닌 JPA에서의 처리처럼만 하고 이후에 SDJ를 활용한 깔끔한 처리
+
+        String itemName = cond.getItemName();
+        Integer maxPrice = cond.getMaxPrice();
+
+        if (StringUtils.hasText(itemName) && maxPrice != null) {
+            //return repository.findByItemNameLikeAndPriceLessThanEqual(itemName, maxPrice);
+            return repository.findItems("%" + itemName + "%", maxPrice);
+        } else if (StringUtils.hasText(itemName)) {
+            return repository.findByItemNameLike("%" + itemName + "%");
+        } else if (maxPrice != null) {
+            return repository.findByPriceLessThanEqual(maxPrice);
+        } else {
+            return repository.findAll();
+            // findAll()은 JpaRepository 인터페이스의 메서드
+        }
+    }
+}
---
+++ itemservice-db/src/main/java/hello/itemservice/repository/jpa/SpringDataJpaItemRepository.java
@@ -0,0 +1,35 @@
+package hello.itemservice.repository.jpa;
+
+import hello.itemservice.domain.Item;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
+
+// *** SDJ는 Interface를 기반으로 작성한다
+public interface SpringDataJpaItemRepository extends JpaRepository<Item, Long> {
+    // 1st : 관리할 Entity
+    // 2nd : Entity의 PK의 Type
+    // -> 기본적인 CRUD는 완성 -> 필요한 기능만 작성
+
+    List<Item> findByItemNameLike(String itemName);
+
+    List<Item> findByPriceLessThanEqual(Integer price);
+
+    // 위의 두 조건을 합친 쿼리 메서드 -> Join 사용 불가능
+    List<Item> findByItemNameLikeAndPriceLessThanEqual(String itemName, Integer price);
+
+    // 쿼리 직접 실행 - jpql 직접 작성한 것과 마찬가지 + 위의 쿼리 메서드와 동일한 기능 수행
+    @Query("select i from Item i where i.itemName like :itemName and i.price <= :price")
+    List<Item> findItems(@Param("itemName") String itemName, @Param("price") Integer price);
+    // *** @Param의 패키지 확인
+    // *** 이렇게 쿼리를 직접 실행할 경우 - Parameter는 명시적으로 @Param 어노테이션을 통해 바인딩 해줘야 함
+    // -> 문자열은 이름 기반 parameter Binding과 매핑됨 ex) "itemName"의 parameter -> :itemName
+
+    // *** 현재의 조회기능을 메서드로 작성한 현황
+    // 모든 데이터 / 이름 / 가격 / 이름 + 가격
+    // -> 동적 쿼리를 사용해서 이를 한번에 묶을 수는 없을까? -> SDJ는 jpql동적 쿼리에 취약
+    // -> 이후 Querydls로 깔끔하게 해결할 예정
+
+}
